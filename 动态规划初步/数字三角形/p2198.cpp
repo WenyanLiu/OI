ed928d7ef28cbd6e04fb8b84485ed99709fd05d0
Rockie <f.v.rockie@hotmail.com>
2016-04-21 11:13:18 +0800

    AC p2198

--- 动态规划初步/数字三角形/p2198.cpp
+++ 动态规划初步/数字三角形/p2198.cpp
@@ -1,56 +1,39 @@
 #include <iostream>
-using namespace std;
+#include <cstring>
 #define MAXN 100
+#define ADD (222*222)
+using namespace std;
+
+int N;
+int a[MAXN][MAXN], x, y;
+int d[MAXN][MAXN];
+
+int p(int i, int j) {
+    
+    if (d[i][j] >= 0) {
+        return d[i][j];
+    }
+    return d[i][j] = a[i][j] + (i == N-1 ? 0 : max(p(i+1,j), p(i+1,j+1)));
+}
 
 int p2198() {
     
-    int N;
-    cin >> N;
+    memset(d, -1, sizeof(d));
     
-    int tree[MAXN][MAXN];
-    int v[MAXN][MAXN] = {0};
+    cin >> N;
     
     int i, j;
     for (i = 0; i < N; i++) {
         for (j = 0; j <= i; j++) {
-            cin >> tree[i][j];
+            cin >> a[i][j];
         }
     }
     
-    int pass_i, pass_j;
-    cin >> pass_i >> pass_j;
+    cin >> x >> y;
     
-    pass_i--;
-    pass_j--;
-    int v_l, v_r;
-    v[0][0] = tree[0][0];
-    for (i = 1; i < N; i++) {
-        for (j = 0; j <= i; j++) {
-            if (i != pass_i+1) {
-                if (j-1 >= 0) {
-                    v_l = v[i-1][j-1];
-                } else {
-                    v_l = -65535;
-                }
-                if (j != i) {
-                    v_r = v[i-1][j];
-                } else {
-                    v_r = -65535;
-                }
-                v[i][j] = tree[i][j] + (v_l > v_r ? v_l : v_r);
-            } else {
-                v[i][pass_j] = tree[i][pass_j] + v[pass_i][pass_j];
-                v[i][pass_j+1] = tree[i][pass_j+1] + v[pass_i][pass_j];
-                break;
-            }
-        }
-    }
+    a[x][y] = a[--x][--y] + ADD;
     
-    int max = -65535;
-    for (j = pass_j; j < pass_j+(N-pass_i); j++) {
-        max = max > v[N-1][j] ? max : v[N-1][j];
-    }
+    cout << p(0, 0) - ADD << endl;
     
-    cout << max;
     return 0;
 }
